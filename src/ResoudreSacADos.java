b9334318908737e14808ad7b853e29a24e9bb59e
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2021-10-15 15:13:15 +0200

    Update ResoudreSacADos

--- src/ResoudreSacADos.java
+++ src/ResoudreSacADos.java
@@ -1,31 +1,30 @@
 public class ResoudreSacADos {
     public static void main(String[] args) {
         SacADos sacADos = new SacADos(args[0], Float.parseFloat(args[1]));
-         long begin ;
+        long begin;
         long end;
 
-      
         switch (args[2]){
             case "gloutonne":
                 begin = System.nanoTime();
                 sacADos.gloutonne();
                 end = System.nanoTime();
-                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +"  ms");
+                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +" ms.");
                 break;
             case "prog.dynamique":
                 begin = System.nanoTime();
                 sacADos.exactesDynamique();
                 end = System.nanoTime();
-                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +"  ms");
+                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +" ms.");
                 break;
             case "prog.pse":
                 begin = System.nanoTime();
                 sacADos.exactesPSE();
                 end = System.nanoTime();
-                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +"  ms");
+                System.out.println("durée d'exécution du programme " +  (end - begin) / 1_000_000.0 +" ms.");
                 break;
             default:
-                System.out.println("Parametre non reconnue, voici les options:\n- gloutonne\n- prog.dynamique\n- prog.pse");
+                System.out.println("Paramètre non reconnue, voici les options:\n- gloutonne\n- prog.dynamique\n- prog.pse");
         }
     }
 }
